cce319294bbf947e1a33f05489ce33410380d47b
Matthew Wolff <mwolff3@lsu.edu>
2016-03-11 00:00:07 -0600

    Test 1 complete

--- Semant/Semant.java
+++ Semant/Semant.java
@@ -61,11 +61,38 @@ public class Semant {
       result = new ExpTy(null, STRING);
     else if (e instanceof Absyn.NilExp)
       result = new ExpTy(null, NIL);
-    else throw new Error("Semant.transExp");
+    else if (e instanceof Absyn.ArrayExp)
+      result = transExp((Absyn.ArrayExp)e);
+    else if (e instanceof Absyn.VarExp)
+      result = transExp((Absyn.VarExp)e);
+    else throw new Error("Failed for "+e.getClass().getName());
     e.type = result.ty;
     return result;
   }
 
+  ExpTy transExp(Absyn.ArrayExp e) {
+    //extract and typecheck the array information
+    Types.NAME type = (Types.NAME)env.tenv.get(e.typ);
+    ExpTy size = transExp(e.size);
+    ExpTy init = transExp(e.init);
+
+    return new ExpTy(null, type);
+  }
+
+  ExpTy transExp(Absyn.VarExp e) {
+    //Likely will have to split into overloaded methods?
+    Absyn.Var var = e.var;
+    Types.NAME type;
+    Entry entry;
+    //REMEMBER: Entries are put into the venv
+    if(var instanceof Absyn.SimpleVar) {
+        entry = (Entry)env.venv.get(((Absyn.SimpleVar)var).name);
+        return new ExpTy(null, ((VarEntry)entry).ty);
+    } else { 
+        throw new Error("varExp "+var.getClass().getName());
+    }
+  }
+  
   ExpTy transExp(Absyn.IfExp e) {
     ExpTy test = transExp(e.test);
     //check for int
@@ -137,7 +164,46 @@ public class Semant {
   Exp transDec(Absyn.Dec d) {
     if (d instanceof Absyn.VarDec)
       return transDec((Absyn.VarDec)d);
-    throw new Error("Semant.transDec");
+    if (d instanceof Absyn.TypeDec)
+      return transDec((Absyn.TypeDec)d);
+    throw new Error("Failed for "+d.getClass().getName());
+  }
+
+  Exp transDec(Absyn.TypeDec d) {
+    System.out.println(d.ty.getClass().getName());
+    //Make a NAME for this type
+    Types.NAME name = new Types.NAME(d.name);
+    //Bind the name to the type that is declared
+    name.bind(transTy(d.ty));
+    d.entry = name;
+    //TODO: ERROR
+    env.tenv.put(d.name, name);
+    //TOOD: go down the TypeDec list....
+    return null;
+  }
+
+  Types.Type transTy(Absyn.Ty t) {
+    //using instanceof, translate param Absyn.Ty into appropriate Types.Type
+    if(t instanceof Absyn.RecordTy)
+      return transTy((Absyn.RecordTy) t);
+    if(t instanceof Absyn.ArrayTy)
+      return transTy((Absyn.ArrayTy) t);
+    throw new Error("Failed for "+t.getClass().getName());
+  }
+
+  Types.ARRAY transTy(Absyn.ArrayTy t) {
+    //look up the type of the array in the type environment
+    Types.Type type = (Types.Type)env.tenv.get(t.typ);
+    //if the lookup fails, something bad happened
+    if(type == null) {
+      error(t.pos, "array type not known");
+      return null;
+    }
+    return new Types.ARRAY(type);
+  }
+
+  Types.RECORD transTy(Absyn.RecordTy t) {
+    throw new Error("Semant.transTy for records unimplemented");
   }
 
   Exp transDec(Absyn.VarDec d) {
@@ -146,10 +212,14 @@ public class Semant {
     ExpTy init = transExp(d.init);
     Type type;
     if (d.typ == null) {
+      //Unknown type? i.e. recursive type?
       type = init.ty;
     } else {
+      type = init.ty;
+      /**
       type = VOID;
-      throw new Error("unimplemented");
+      throw new Error("unimplemented declaration: "+d.typ.getClass().getName());
+      */
     }
     d.entry = new VarEntry(type);
     env.venv.put(d.name, d.entry);
